2d693757951c9a95211574573770667cdeccaad2
Sayolight <sayolight@proton.me>
2025-11-30 19:11:27 +0100

    failed checkout

---
+++ xwear-frontend/src/app/checkout/cancel/page.tsx
@@ -0,0 +1,8 @@
+export default function SuccessPage() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold mb-4">Помилка!</h1>
+            <p className="text-lg">Ваше замовлення не було оформлене.</p>
+        </div>
+    );
+}
